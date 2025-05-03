2c124a62cc6e3bab295655a48810a3ba9520b6c7
zhang-yish <3556645295@qq.com>
2025-05-04 00:39:29 +0800

    更新了关于namespace的相关使用内容

--- C++学习/为什么不建议使用using namespace std/命名空间namespace.cpp
+++ C++学习/为什么不建议使用using namespace std/命名空间namespace.cpp
@@ -8,13 +8,24 @@ void apple_print(const std::string& text)
 	std::cout << text << std::endl;
 }//传统的C语言中没有命名空间的写法应该是这样的，可以避免命名冲突
 
-namespace apple { namespace function {
+namespace apple1 { namespace function {
 //不按照VS自动的缩进方式(大括号不换行)，而是这样写可以在发生多级命名空间嵌套是，将内容一级缩进、二级缩进这样看很清晰看清
 		void print(const char* text)
 		{
 			std::cout << text << std::endl;
 		}
+		
 	}
+}
+
+namespace apple {
+
+	void print(const char* text)
+	{
+			std::cout << text << std::endl;
+	}
+
+	void print_again();
 }
 
 namespace orange {
@@ -31,8 +42,22 @@ namespace orange {
 
 int main()
 {
-	apple::function::print("Hello");//::是命名空间操作符，写上就意味着进入了这个命名空间
+	apple1::function::print("Hello");
 
+	apple::print("Hello");//::是命名空间操作符，写上就意味着进入了这个命名空间
+//同样的方法也可以用在类中，因为类本身就是命名空间
+
+	/*using namespace orange/apple;可以这样方便修改
+	print("Hello");*/
+	using apple::print;//只单独使用apple中的print函数
+	print("Hello");
+	apple::print_again();//这里依然需要指明print_again()来自哪个命名空间
+
+	namespace a = apple;
+	a::print("Hello");//可以给apple命名空间一个别名
+
+	namespace b = apple1::function;
+	b::print("Hello");
 
 
 	std::cin.get();
