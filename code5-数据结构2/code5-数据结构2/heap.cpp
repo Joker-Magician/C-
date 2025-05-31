35a957bf4031eb1ddb68da415d40fca14c64eabe
zhang-yish <3556645295@qq.com>
2025-05-31 23:49:49 +0800

    更新了关于堆的相关内容

---
+++ code5-数据结构2/code5-数据结构2/heap.cpp
@@ -0,0 +1,37 @@
+#define _CRT_SECURE_NO_WARNINGS
+//堆，即优先队列（Priority Queue),特殊的队列，取出元素的顺序是依照元素的优先权(关键字)大小，而不是元素进入队列的先后顺序
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+#define MaxData 100
+
+typedef int ElementType;
+
+typedef struct HeapStruct* MaxHeap;
+struct HeapStruct {
+	ElementType* Elements;	/*存储堆元素的数组*/
+	int Size;				/*堆的当前元素个数*/
+	int Capacity;			/*堆的最大容量*/
+};
+
+MaxHeap Creat(int MaxSize)
+{		/*创建容易为MaxSize的空的最大堆(大顶堆)*/
+	MaxHeap H = (MaxHeap)malloc(sizeof(struct HeapStruct));
+	H->Elements = (ElementType*)malloc((MaxSize + 1) * sizeof(ElementType));
+	H->Size = 0;
+	H->Capacity = MaxSize;
+	H->Elements[0] = MaxData;
+	/*定义“哨兵”为大于堆中所有可能元素的值，以便于以后更快操作*/
+	return H;
+}
+
+int main()
+{
+
+
+	return 0;
+}
+
+
+//采用二叉搜索树存储堆，这个树是一个完全二叉树
