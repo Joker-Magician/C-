17930092ca0b946ea1dca508c0405f9db2a821eb
zhang-yish <3556645295@qq.com>
2025-06-01 22:19:31 +0800

    更新了关于最大堆的相关内容

--- code5-数据结构2/code5-数据结构2/heap.cpp
+++ code5-数据结构2/code5-数据结构2/heap.cpp
@@ -26,6 +26,26 @@ MaxHeap Creat(int MaxSize)
 	return H;
 }
 
+int IsFull(MaxHeap H)
+{
+
+}
+
+void Insert(MaxHeap H, ElementType item)
+{	/* 将元素item插入最大堆H,其中H->Elements[0]已经定义为哨兵 */
+	int i;
+	if (IsFull(H))
+	{
+		printf("最大堆已满");
+		return;
+	}
+	i = ++H->Size;/* i指向插入后堆中的最后一个元素的位置 */
+	for (; H->Elements[i / 2] < item; i /= 2)
+		H->Elements[i] = H->Elements[i / 2];/* 向下过滤节点 */
+	H->Elements[i] = item;	/* 将item插入 */
+}
+
+
 int main()
 {
 
