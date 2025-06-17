5ea7fff19a8272ba6bb9c563a8c50d329442dffe
zhang-yish <3556645295@qq.com>
2025-06-17 23:30:24 +0800

    更新了关于链表的相关内容

--- code5-数据结构2/线性表--链表/链表LinkList.cpp
+++ code5-数据结构2/线性表--链表/链表LinkList.cpp
@@ -146,40 +146,71 @@ int DelList(linklist L, int* pos, int e)
 	return OK;
 }
 
-void Mergelinklist(linklist LA, linklist LB)
-/*将递增有序的单链表LA和LB合并成一个递增有序的单链表*/
+//void Mergelinklist(linklist LA, linklist LB)
+// //*将递增有序的单链表LA和LB合并成一个递增有序的单链表*/
+//{
+//	Node* pa, * pb;
+//	linklist r;
+//	/*pa和pb分别指向两个单链表LA和LB中的第一个结点*/
+//	pa = LA->next;
+//	pb = LB->next;
+//
+//	r = LA;
+//	r->next = NULL;/*r指向空表*/
+//
+//	/*当两个表中均未处理完时，比较选择将较小值结点插入到新表中。*/
+//	while (pa != NULL && pb != NULL)
+//	{
+//		if (pa->data <= pb->data)
+//		{
+//			r->next = pa;
+//			r = pa;
+//			pa = pa->next;
+//		}
+//		else
+//		{
+//			r->next = pb;
+//			r = pb;
+//			pb = pb->next;
+//		}
+//	}
+//	if (pa) /*若表LA未完，将表LA中后续元素链到新表表尾*/
+//		r->next = pa;
+//	else	 /*否则将表LB中后续元素链到新表表尾*/
+//		r->next = pb;
+//	free(LB);
+//}  /* Mergelinklist */
+
+linklist  Mergelinklist(linklist LA, linklist LB)
+/*将递增有序的单链表LA和LB合并成一个递增有序的单链表LC*/
 {
 	Node* pa, * pb;
-	linklist r;
-	/*pa和pb分别指向两个单链表LA和LB中的第一个结点*/
+	linklist LC, r;
+	/*将LC初始置空表。pa和pb分别指向两个单链表LA和LB中的第一个结点,r初值为LC*/
 	pa = LA->next;
 	pb = LB->next;
-
-	r = LA;
-	r->next = NULL;/*r指向空表*/
-
-	/*当两个表中均未处理完时，比较选择将较小值结点插入到新表中。*/
+	LC = LA;
+	LC->next = NULL; r = LC;							/*初始化操作*/
+	/*当两个表中均未处理完时，比较选择将较小值结点插入到新表LC中。*/
 	while (pa != NULL && pb != NULL)
 	{
 		if (pa->data <= pb->data)
 		{
-			r->next = pa;
-			r = pa;
-			pa = pa->next;
+			r->next = pa; r = pa; pa = pa->next;
 		}
 		else
 		{
-			r->next = pb;
-			r = pb;
-			pb = pb->next;
+			r->next = pb; r = pb; pb = pb->next;
 		}
 	}
-	if (pa) /*若表LA未完，将表LA中后续元素链到新表表尾*/
+	if (pa) /*若表LA未完，将表LA中后续元素链到新表LC表尾*/
 		r->next = pa;
-	else	 /*否则将表LB中后续元素链到新表表尾*/
+	else	 /*否则将表LB中后续元素链到新表LC表尾*/
 		r->next = pb;
+
 	free(LB);
-}  /* Mergelinklist */
+	return(LC);
+}
 
 int main()
 {
