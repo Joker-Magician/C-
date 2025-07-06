d28ccdc6df87cb78702a95329543aa73617f74b7
zhang-yish <3556645295@qq.com>
2025-07-06 17:26:56 +0800

    更新了关于OpenGL配置相关内容

--- OpenGL/OpenGL/src/Application.cpp
+++ OpenGL/OpenGL/src/Application.cpp
@@ -1,4 +1,7 @@
+#include <GL/glew.h>    //注意在包含任何其他OpenGL相关的头文件之前就要包含glew.h，不然会发生一些命名冲突
 #include <GLFW/glfw3.h>
+
+#include <iostream>
 
 int main(void)
 {
@@ -16,14 +19,29 @@ int main(void)
         return -1;
     }
 
+    /*if (glewInit() != GLEW_OK) 在这里初始化是错误的，想要正确的初始化需要有一个渲染OpenGL的有效上下文后面
+        std::cout << "Error!" << std::endl;
+        */
+
     /* Make the window's context current */
     glfwMakeContextCurrent(window);
+
+    if (glewInit() != GLEW_OK)
+        std::cout << "Error!" << std::endl;
+
+    std::cout << glGetString(GL_VERSION) << std::endl;
 
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
     {
         /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
+
+        glBegin(GL_TRIANGLES);
+        glVertex2f(-0.5f, -0.5f);
+        glVertex2f( 0.5f,  0.5f);
+        glVertex2f( 0.5f, -0.5f);
+        glEnd();
 
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
@@ -35,3 +53,8 @@ int main(void)
     glfwTerminate();
     return 0;
 }
+
+
+
+// warning LNK4098: 默认库“MSVCRT”与其他库的使用冲突；请使用 /NODEFAULTLIB:library
+//出现该错误是在静态链接上出现了错误，因为这里需要的是动态链接，本代码中使用的是静态链接(在任何情况下都推荐使用静态链接)，所以需要在预处理器中提前定义GLEW_STATIC
