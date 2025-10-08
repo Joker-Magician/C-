db1eb8de9481f6dcc680b8cb813daa765d009bde
zhang-yish <3556645295@qq.com>
2025-10-08 23:26:54 +0800

    更新了关于OpenGL创建纹理测试的相关内容

--- OpenGL/Test/src/Application.cpp
+++ OpenGL/Test/src/Application.cpp
@@ -22,6 +22,7 @@
 #include "imgui/imgui_impl_glfw_gl3.h"
 
 #include "tests/TestClearColor.h"
+#include "tests/TestTexture2D.h"
 
 int main(void)
 {
@@ -67,6 +68,7 @@ int main(void)
 		currentTest = testMenu;
 		
 		testMenu->RegisterTest<test::TestClearColor>("Clear Color"); // ×¢²á²âÊÔ
+		testMenu->RegisterTest<test::TestTexture2D>("2D Texture"); // ×¢²á²âÊÔ
 
 		while (!glfwWindowShouldClose(window))
 		{
